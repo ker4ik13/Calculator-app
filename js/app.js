bff8d601aa009c2b7ab3d2a5751408ad75c4034e
ker4ik13 <kekustop228@mail.ru>
2023-03-16 14:58:44 +0300

    Fix some bugs, remake interface and bg

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ let orangeButtons = document.querySelectorAll('.orange__button'),
 const digit = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const action = ['+', '–', '÷', 'x'];
 
-
 /* Clear */
 function clearAll(){
     a = '';
@@ -36,15 +35,20 @@ resultField.addEventListener('click', () => {
     if(a !== '' && b === ''){
         a = a.slice(0, -1);
         resultField.innerHTML = a;
-        console.log(a);
+        if(a === '' && b === '') {
+            a = '0';
+            resultField.innerHTML = '0';
+        }
     }
     if(b !== '' && a !== ''){
         b = b.slice(0, -1);
         resultField.innerHTML = b;
-        console.log(a);
+        if(a !== '' && b === '') {
+            b = '0';
+            resultField.innerHTML = '0';
+        }
     }
 })
-
 
 container.addEventListener('click', (event) => {
     const key = event.target.textContent;
@@ -59,9 +63,8 @@ container.addEventListener('click', (event) => {
         if(key === '0' && (a === '' || a === '0') && b === ''){
             resultField.innerHTML = '0';
         } else if(b === '' && sign === ''){
-            a+=key;
+            a += key;
             resultField.innerHTML = a;
-            
             // Удаление класса orange buttons
             for(let i = 0; i < container.children.length; i++){
                 container.children[i].classList.remove('active');
@@ -82,7 +85,88 @@ container.addEventListener('click', (event) => {
         // Если нажаты + - / *
      if(action.includes(key)){
         sign = key;
-        resultField.innerHTML = a;
+        if(a === '' || a === '0'){
+            resultField.innerHTML = '0';
+        } else if(a !== ''){
+            resultField.innerHTML = a;
+        }
+            // Вычисления +
+        if(sign === '+'){
+            if(a !=='' && b === ''){
+                resultField.innerHTML = a;
+                a = Number(a);
+            } else if(a !=='' && b !== ''){
+                a = Number(a) + Number(b);
+                resultField.innerHTML = a;
+            } else if(a !=='' && b !== '' && finish){
+                b = '';
+                a = Number(a) + Number(b);
+                resultField.innerHTML = a;
+                finish = true;
+            } 
+        b = '';
+        finish = true;
+        }
+        
+            // Вычисления -
+        if(sign === '–'){
+            if(a !=='' && b === ''){
+                resultField.innerHTML = a;
+                a = Number(a);
+            } else if(a !=='' && b !== ''){
+                a = Number(a) - Number(b);
+                resultField.innerHTML = a;
+                b = '';
+                finish = true;
+            } else if(a !=='' && b !== '' && finish){
+                b = '';
+                a = Number(a) - Number(b);
+                resultField.innerHTML = a;
+                finish = true;
+            } 
+        b = '';
+        finish = true;
+        }
+        
+            // Вычисления x
+        if(sign === 'x'){
+            if(a !=='' && b === ''){
+                resultField.innerHTML = a;
+                a = Number(a);
+            } else if(a !=='' && b !== ''){
+                a = Number(a) * Number(b);
+                resultField.innerHTML = a;
+                b = '';
+                finish = true;
+            } else if(a !=='' && b !== '' && finish){
+                b = '';
+                a = Number(a) * Number(b);
+                resultField.innerHTML = a;
+                finish = true;
+            }
+        b = '';
+        finish = true;
+        }
+
+            // Вычисления /
+        if(sign === '÷'){
+            if(a !=='' && b === ''){
+                resultField.innerHTML = a;
+                a = Number(a);
+            } else if(sa !=='' && b !== ''){
+                a = Number(a) / Number(b);
+                resultField.innerHTML = a;
+                b = '';
+            } else if(a !=='' && b !== '' && finish){
+                b = '';
+                a = Number(a) / Number(b);
+                resultField.innerHTML = a;
+                finish = true;
+            }
+        b = '';
+        finish = true;
+        }
+        
 
         for(let i = 0; i < container.children.length; i++){
             container.children[i].classList.remove('active');
@@ -92,10 +176,11 @@ container.addEventListener('click', (event) => {
             sign = key;
             resultField.innerHTML = a;
         }
+        console.log(a, b, sign);
         return;
     }  
+     
     // Равно
-
     if(key === '═'){
         if(b==='') b = a;
         switch(sign){
